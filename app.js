afd945bb72b932357f6d08d7c1f5c1dc89f568d5
BLK Garut <you@example.com>
2025-09-19 10:22:37 +0700

    Webserver

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const http = require('http');
-const port =3000;
-const server = http.createServer((req, res) => {
-    res.writeHead(200,{
-        'Content-Type': 'text/html'
-    });
-    res.write('<h1>Hello World</h1>');
-    res.end();
+// const http = require('http');
+// const port =3000;
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200,{
+//         'Content-Type': 'text/html'
+//     });
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// });
+// server.listen(port,() => {
+// console.log(`Server running at ${port}...`);
+// });
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', {root:__dirname});
 });
-server.listen(port,() => {
-console.log(`Server running at ${port}...`);
+
+app.get('/about', (req, res) => {
+    res.sendFile('./about.html', {root:__dirname});
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile('./contact.html', {root:__dirname});
+});
+
+app.get('/product/:id', (req, res) => {
+    res.send(`Product ID: ${req.params.id} <br> Category ID: ${req.query.idCat}`);
+});
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h1>Test</h1>');
+});
+
+app.listen(port,() => {
+    console.log(`Exaple app listening port ${port}`);
 });
